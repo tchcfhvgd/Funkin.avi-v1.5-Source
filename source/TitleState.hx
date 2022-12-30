255e1fc32c56742a99748a71081c6525bffe7dee
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-12-30 19:53:48 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -103,8 +103,9 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
-		GameJolt.GameJoltAPI.connect();
-        GameJolt.GameJoltAPI.authDaUser(FlxG.save.data.gjUser, FlxG.save.data.gjToken);
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
 
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
@@ -428,7 +429,7 @@ class TitleState extends MusicBeatState
 
 	public static function restartGame()
 		{
-			#if cpp
+			#if windows 
 			var os = Sys.systemName();
 			var args = "Test.hx";
 			var app = "";

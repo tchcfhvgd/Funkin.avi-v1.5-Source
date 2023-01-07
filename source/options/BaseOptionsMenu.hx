0b23700b91299634b865e458f138367363939edb
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-01-06 22:52:41 -0300

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -141,6 +141,11 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		grain.scale.x = 1.1;
 		grain.scale.y = 1.1;
 		add(grain);
+		
+		#if android
+		addVirtualPad(LEFT_FULL, A_B_C);
+		addPadCamera();
+		#end
 	}
 
 	public function addOption(option:Option) {
@@ -163,7 +168,12 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		}
 
 		if (controls.BACK) {
-			close();
+			#if android
+                        flixel.addons.transition.FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+                        #else
+                        close();
+                        #end
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
 
